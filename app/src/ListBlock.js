bebf49d65e9c4be3163d7d95cdc978b1e022a747
ah1096 <alyssa.holbert@student.awesomeinc.org>
2022-10-19 19:54:56 +0000

    cleaned up hard-coding on ListBlock

--- app/src/ListBlock.js
+++ app/src/ListBlock.js
@@ -10,20 +10,6 @@ export default function ListBlock(){
         <div class="col">
         <ul class="list-group">
 
-            {/* <li class="list-group-item d-flex align-items-center justify-content-between">
-
-                <div>
-                    <input class="form-check-input me-1" type="checkbox" value="" id="firstCheckbox"/>
-                        <label id="listItemName" class="form-check-label" for="firstCheckbox">    
-                            eat bugs
-                        </label>
-                </div>
-                
-                    <div class="align-self-end">
-                        <button class="btn btn-danger" type="button" id="deleteItemBtn">x</button>
-                    </div>
-            </li> */}
-
             <ListItem />
 
             <ListItem />
