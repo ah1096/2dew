ed52a6f35cf9294636a8cf30a56add96600b8372
ah1096 <arh.10.96@gmail.com>
2023-04-28 12:07:16 +0000

    minor fixes

--- app/src/InputBlock.js
+++ app/src/InputBlock.js
@@ -1,50 +1,45 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
+export default function InputBlock({ tasks, setTask }) {
+  const [item, setItem] = useState("");
 
-export default function InputBlock({ tasks, setTask }){
+  function addTask() {
+    setTask([
+      ...tasks,
+      { id: Date.now(), item: item }
+    ]);
+    setItem("");
+  }
 
-    const [item, setItem] = useState('');
+  function handleKeyDown(e) {
+    if (e.keyCode === 13) {
+      addTask();
+    }
+  }
 
+  return (
+    <div id="inputBlock" className="text-center">
+      <h1>To Dew List</h1>
+      <div id="addTask" className="input-group mb-3">
+        <input
+          id="itemInput"
+          type="text"
+          className="form-control"
+          placeholder="add a task"
+          value={item}
+          onChange={(e) => setItem(e.target.value)}
+          onKeyDown={handleKeyDown}
+        />
 
-        return (
-
-            <div id="inputBlock" className="text-center">
-
-                <h1>To Dew List</h1>
-
-                <div id="addTask" className="input-group mb-3">
-                    <input 
-                        id="itemInput" 
-                        type="text" 
-                        className="form-control" 
-                        placeholder="add a task" 
-                        value = {item}
-                        onChange={e => setItem(e.target.value)}
-                    />
-
-                    <button 
-                        className="btn btn-success" 
-                        type="button" 
-                        id="addItemBtn"
-                        onClick={()=> {
-                            setItem('');
-                            setTask([
-                                ...tasks,
-                                { id: Date.now(), item: item}
-                            ])
-                            localstorage.addItem
-                        }}
-                        >
-                        +
-                    </button>
-
-                    
-                    
-
-
-                    </div>
-
-            </div>
-
-       )};
-        
+        <button
+          className="btn btn-success"
+          type="button"
+          id="addItemBtn"
+          onClick={addTask}
+        >
+          +
+        </button>
+      </div>
+    </div>
+  );
+}
--- app/src/ListItem.js
+++ app/src/ListItem.js
@@ -1,80 +1,38 @@
+import { useState } from "react";
 
-export default function ListItem({ item, id, setTask, tasks, setItem, status, setStatus }){
+export default function ListItem({ item, id, setTask, tasks }) {
+  const [status, setStatus] = useState("active");
 
+  const handleCheckboxClick = () => {
+    setStatus(status === "active" ? "done" : "active");
+  };
 
-    const handleClick = (id, e) => {
-        let checkbox = e.target
-        if (checkbox.checked === true) {
-            setItem(item.map(fwog => fwog.id === id ? {...fwog, status: active} : fwog))
-            checkbox.checked = true 
-        } else {
-            setItem(item.map(fwog => item.id === id ? {...fwog, statu: done} : fwog))
-            checkbox.checked = false
-        }
-        }
+  const handleDeleteClick = () => {
+    setTask(tasks.filter((task) => task.id !== id));
+  };
 
+  return (
+    <li className={`list-group-item d-flex align-items-center justify-content-between ${status}`}>
+      <div>
+        <input
+          className="form-check-input me-1"
+          type="checkbox"
+          value=""
+          id={`checkbox-${id}`}
+          onClick={handleCheckboxClick}
+          checked={status === "done"}
+        />
 
+        <label className="form-check-label" htmlFor={`checkbox-${id}`}>
+          {item}
+        </label>
+      </div>
 
-
-
-
-
-    return (
-        <li className="list-group-item d-flex align-items-center justify-content-between">
-
-                <div>
-                    <input 
-                        className="form-check-input me-1" 
-                        type="checkbox" 
-                        value="" 
-                        id="firstCheckbox"
-
-
-// checkboxes don't toggle back and forth both active -> done AND done -> active                     
-                        onClick={(e) => handleClick(item.id, e)}
-                        
-                            // if (status === "active"){
-                            //     status = setStatus("done")
-                            // }
-
-                            // let clickCount = 2
-                
-                            // if (clickCount % 2 === 0){
-                            // setStatus(status = "done")
-                            // }else{setStatus(status = "active")};
-
-                            // clickCount++
-                            // console.log(clickCount);
-                        // }}
-                            />
-
-                        <label  
-                            className="form-check-label" 
-                            htmlFor="firstCheckbox"
-                            item={item}
-                            id={id}
-                            status={status}
-                            >    
-                                {item}
-                        </label>
-
-                </div>
-                
-                    <div className="align-self-end">
-                        <button 
-                            className="btn btn-danger" 
-                            type="button" 
-                            id="deleteItemBtn"
-                            onClick={() => {
-                                    setTask(tasks.filter(a =>
-                                        a.id !== id))
-                            }}
-
-
-                            >x</button>
-                    </div>
-            </li>
-    )
+      <div className="align-self-end">
+        <button className="btn btn-danger" type="button" id="deleteItemBtn" onClick={handleDeleteClick}>
+          x
+        </button>
+      </div>
+    </li>
+  );
 }
-
-                        
